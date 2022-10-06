fc78de17d29a0496301cdac3e3ccb7b59345141b
Le Minh Nhat Dang <lmndang.toronto@gmail.com>
2022-10-06 00:17:44 -0400

    Reading the file

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const { parse } = require("csv-parser");
+const fs = require("fs");
+
+const results = [];
+
+//Reading the file line by line
+//Push the buffer data into the result array
+//Even Emitter
+fs.createReadStream("./kepler_data.csv")
+  .on("data", (data) => {
+    results.push(data);
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(results);
+    console.log("done");
+  });
