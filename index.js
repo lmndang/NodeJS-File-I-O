e2b2c7cf0daaab17a0b9997ae12337814ebd8277
Le Minh Nhat Dang <lmndang.toronto@gmail.com>
2022-10-06 00:49:32 -0400

    Finding habitable planets

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const { parse } = require("csv-parse");
 const fs = require("fs");
 
-const results = [];
+//const results = [];
+
+const habitablePlanet = [];
+
+const isHabitablePlanet = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
 
 //Reading the file line by line
 //Push the buffer data into the result array
@@ -15,12 +26,14 @@ fs.createReadStream("./kepler_data.csv")
     })
   )
   .on("data", (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      habitablePlanet.push(data);
+    }
   })
   .on("error", (err) => {
     console.log(err);
   })
   .on("end", () => {
-    console.log(results.length);
+    console.log(habitablePlanet.length);
     console.log("done");
   });
